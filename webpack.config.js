3e3e3144bb4f28416ea932c83a7387726cd84d35
j_lyons2@u.pacific.edu <j_lyons2@u.pacific.edu>
2020-01-03 18:05:27 -0800

    2. adds babel settings to package.json and configures webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js',
+        publicPath: '/'
+    },
+    modules: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader' },
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+        ]
+    },
+    mode: process.env_NODE_ENV === 'production' ? 'production' : 'development',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+        new CopyPlugin([
+            { from: 'public/_redirects' }
+        ])
+    ],
+    devServer: {
+        historyApiFallback: true
+    }
+};
